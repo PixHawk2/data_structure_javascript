96a7dcf52b7aa92b16a9e1d1f2741e278e22f8d9
hudi <wangqian1107@163.com>
2022-01-01 16:12:45 +0800

    双向链表实现

---
+++ doublelist.js
@@ -0,0 +1,204 @@
+/*单个节点实现*/
+class Node{
+	constructor(element1){
+		this.element = element1;
+		this.next = null;
+	}
+}
+/*单向链表实现*/
+class LinkedList{
+	constructor(){
+		this.count = 0;
+		this.head = null;
+	}
+	//链表尾部新增元素
+	push(element1){
+		const node =  new Node(element1);
+		let current;
+		if(this.head === null){
+			this.head = node;
+		}else{
+			current = this.head;
+			while(current.next !== null){
+				current = current.next;
+			}
+			current.next = node;
+		}
+		this.count++;
+	}
+	//从链表中移除元素
+	removeAt(index){
+		if(index>=0 && index < this.count){
+			let current = this.head;
+			if(index === 0){
+				this.head = current.next;
+			}else{
+				let previous;
+				/*for(let i =0;i<index;i++){
+					previous = current;
+					current = current.next;
+				}*/
+				previous = this.getElementAt(index-1);
+				current = previous.next;
+				previous.next = current.next
+			}
+			this.count--;
+			return current.element
+		}
+		return undefined
+	}
+	//循环迭代到目标索引位置可以独立出来
+	getElementAt(index){
+		if(index>=0 && index <=this.count){
+			let node = this.head;
+			for(let i = 0;i < index && node !== null;i++){
+				node = node.next;
+			}
+			return node;
+		}
+		return undefined;
+	}
+	//指定位置插入元素
+	insert(element,index){
+		if(index >= 0 && index <= this.count){
+			const node = new Node(element);
+			if(index === 0){
+				const current = this.head;
+				this.head = node;
+				node.next = current;
+			}else{
+				let previous = this.getElementAt(index-1);
+				let current = previous.next;
+				previous.next = node;
+				node.next = current;
+			}
+			this.count++;
+			return true
+		}
+		return false
+	}
+	//查找链表中有无元素
+	indexOf(value){
+		let current = this.head;
+		for(let i=0;i<this.count && current !== null;i++){
+			if(value === current.element){
+				return i;
+			}
+			current = current.next;
+		}
+		return -1;
+	}
+	//移除链表中的某元素
+	remove(element){
+		let index = this.indexOf(element);
+		return this.removeAt(index);
+	}
+	//
+	size(){
+		return this.count;
+	}
+	//
+	isEmpty(){
+		return this.count === 0;
+	}
+	//
+	getHead(){
+		return this.head;
+	}
+	toString(){
+		let objString;
+		if(this.head === null){
+			return '';
+		}else{
+			objString = `${this.head.element}`;
+			let current = this.head.next;
+			for(let i =1;i<this.count&&current !== null;i++){
+				objString = `${objString},${current.element}`;
+				current = current.next;
+			}
+		}
+		return objString;
+	}
+}
+
+class DoubleNode extends Node{
+	constructor(element,next,prev){
+		super(element,next);
+		this.prev = prev;
+	}
+}
+class DoubleLinkedList extends LinkedList{
+	constructor(){
+		super();
+		this.tail = null;
+	}
+	//循环链表的push也需要重写
+	//在任意位置插入新元素
+	insert(element,index){
+		if(index>=0 && index<=this.count){
+			const node = new DoubleNode(element);
+			let current =  this.head;
+			if(index === 0){
+				if(this.head == null){
+					this.head = node;
+					this.tail = node;
+				}else{
+					node.next = this.head;
+					current.prev = node;
+					this.head = node;
+				}
+			}else if(index === this.count){
+				current = this.tail;
+				current.next = node;
+				node.prev = current;
+				this.tail = node;
+			}else{
+				const previous = super.getElementAt(index-1);
+				current =  previous.next;
+				node.next = current;
+				previous.next =  node;
+				current.prev = node;
+				node.prev = previous;
+			}
+			this.count++;
+			return true;
+		}
+		return false;
+	}
+	//在任意位置移除元素
+	removeAt(index){
+		if(index>=0 && index <= this.count){
+			let current = this.head;
+			if(index === 0 ){
+				this.head = current.next;
+				if(this.count === 1){
+					this.tail = undefined;
+				}else{
+					this.head.prev = undefined;
+				}
+			}else if(index === this.count-1){
+				current = this.tail -1;
+				this.tail = current.prev;
+				this.tail.next = undefined;
+			}else{
+				current = super.getElementAt(index);
+				const previous = current.prev;
+				previous.next = current.next;
+				current.next.prev = previous;
+			}
+			this.count--;
+			return current.element
+		}
+		return undefined;
+	}
+	
+}
+let doublelist = new DoubleLinkedList();
+doublelist.insert(100,0);
+doublelist.insert(200,1);
+doublelist.insert(300,1);
+doublelist.insert(700,1);
+console.log(doublelist);
+console.log('*************************************')
+doublelist.removeAt(2);
+console.log(doublelist);
